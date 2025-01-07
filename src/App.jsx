735be64711b7734bd7c892e143c8b38f7a186311
Victor Brouwer <victor.brouwer@hotmail.com>
2025-01-07 17:56:41 +0100

    added color scheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Hero, Navbar, StarsCanvas, Tech, Works } from './components';
-import About from './components/About';
+import { Contact, Hero, Navbar, StarsCanvas, Tech, Works } from './components';
 import Skills from './components/Skills';
-import Experience from './components/Experience';
 import Education from './components/Education';
 import Interests from './components/Interests';
+import About from './components/About';
+import Experience from './components/Experience';
 
-const App = () => {
+function App() {
   return (
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
+    <div className="min-h-screen">
+      <main>
         <About />
         <Skills />
         <Experience />
         <Education />
         <Interests />
-        <Tech />
-        <Works />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
-    </BrowserRouter>
+      </main>
+    </div>
   );
-};
+}
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     extend: {
       colors: {
         primary: '#050816',
-        secondary: '#aaa6c3',
-        tertiary: '#151030',
+        secondary: '#f58b08',
+        tertiary: '#0828f5',
         'black-100': '#100d25',
         'black-200': '#090325',
         'white-100': '#f3f3f3',
